253a6547ee8e46390b23374143d6099b45f0af41
Artem Kabanin <a.kabanin@intetics.com>
2021-01-07 15:42:47 +0200

    Replce deprecated async => waitForAsync

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -1,11 +1,8 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 import {CoursesCardListComponent} from './courses-card-list.component';
 import {CoursesModule} from '../courses.module';
-import {COURSES} from '../../../../server/db-data';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
-import {sortCoursesBySeqNo} from '../home/sort-course-by-seq';
-import {Course} from '../model/course';
 import {setupCourses} from '../common/setup-test-data';
 
 
@@ -15,7 +12,7 @@ describe('CoursesCardListComponent', () => {
   let el: DebugElement;
 
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [CoursesModule],
     }).compileComponents();
